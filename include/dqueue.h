bbf81ecbc08232def1a0fa7bdc200f5e6a8c497b
hammadmajid <hammadmajid@proton.me>
2024-10-27 13:32:37 +0500

    implement dqueue.h

--- include/dqueue.h
+++ include/dqueue.h
@@ -1,29 +1,83 @@
 // Dynamic Queue implemented with linked list
-// Author: Ammad Qureshi 23122--
+// Author: Ammad Qureshi 2312213
 
 #ifndef DQUEUE_H
 #define DQUEUE_H
 
+#include <iostream>
+
 template <typename T>
 class DQueue {
     struct Node {
+        T data;
+        Node *next;
 
+        Node(T val) : data(val), next(nullptr) {}
     };
 
     Node *front;
     Node *rear;
+
 public:
-    DQueue(): front(nullptr), rear(nullptr) {
+    DQueue() : front(nullptr), rear(nullptr) {}
+
+    ~DQueue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
     }
 
-    void enqueue(T data) {}
-    void dequeue() {}
+    void enqueue(T data) {
+        Node *newNode = new Node(data);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
 
-    T getFront() {}
-    T getRear() {}
+    void dequeue() {
+        if (!isEmpty()) {
+            Node *temp = front;
+            front = front->next;
+            if (front == nullptr) {
+                rear = nullptr;
+            }
+            delete temp;
+        } else {
+            std::cerr << "Queue is empty, cannot dequeue." << std::endl;
+        }
+    }
 
-    bool isEmpty() {}
-    void printQueue() {}
+    T getFront() {
+        if (!isEmpty()) {
+            return front->data;
+        } else {
+            throw std::runtime_error("Queue is empty");
+        }
+    }
+
+    T getRear() {
+        if (!isEmpty()) {
+            return rear->data;
+        } else {
+            throw std::runtime_error("Queue is empty");
+        }
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+
+    void printQueue() {
+        Node *current = front;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
 #endif
