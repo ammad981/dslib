14d68cb22e6d28757e8aa19ecf3b5f4c8355110d
hammadmajid <hammadmajid@proton.me>
2024-10-28 02:26:42 +0500

    implement dstack.h

--- include/dstack.h
+++ include/dstack.h
@@ -1,27 +1,77 @@
 // Dynamic Stack implemented with linked list
-// Author: Wajeeh Ahmed 23122--
+// Author: Wajeeh Ahmed 2312223
 
 #ifndef DSTACK_H
 #define DSTACK_H
 
-template <typename T>
+#include <iostream>
+
+template<typename T>
 class DStack {
     struct Node {
+        T data;
+        Node *next;
 
+        Node(T data) : data(data), next(nullptr) {
+        }
     };
 
     Node *top;
+
 public:
-    DStack(): top(nullptr) {
+    DStack() : top(nullptr) {
     }
 
-    void push(T data) {}
-    void pop() {}
+    void push(T data) {
+        Node *newNode = new Node(data);
+        newNode->next = top;
+        top = newNode;
+    }
 
-    T getTop() {}
+    void pop() {
+        if (!isEmpty()) {
+            Node *temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
 
-    bool isEmpty() {}
-    void printStack() {}
+    T getTop() {
+        if (!isEmpty()) {
+            return top->data;
+        }
+        throw std::runtime_error("Stack is empty");
+    }
+
+    bool isEmpty() { return top == nullptr; }
+
+    int search(T target) {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        Node *current = top;
+        int position = 0;
+
+        while (current != nullptr) {
+            if (current->data == target) {
+                return position;
+            }
+            current = current->next;
+            position++;
+        }
+
+        return -1;
+    }
+
+    void printStack() {
+        Node *current = top;
+        while (current) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
 #endif
