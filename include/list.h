a8a68ab8810dc1f792a300984d482045e3d6642f
hammadmajid <hammadmajid@proton.me>
2024-10-28 02:25:40 +0500

    implement list.h

--- include/list.h
+++ include/list.h
@@ -4,26 +4,174 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <typename T>
+#include <iostream>
+
+template<typename T>
 class List {
     struct Node {
+        T data;
+        Node *next;
 
+        Node(T value) : data(value), next(nullptr) {
+        }
     };
 
     Node *head;
+
 public:
-    List(): head(nullptr) {
+    List() : head(nullptr) {
     }
 
-    void insertAtBegin(T data) {}
-    void insertAtEnd(T data) {}
-    void insertAtMiddle(T data, unsigned int position) {}
+    // Destructor to free allocated memory
+    ~List() {
+        while (head != nullptr) {
+            deleteAtBegin();
+        }
+    }
 
-    void deleteAtBegin() {}
-    void deleteAtEnd() {}
-    void deleteAtMiddle(unsigned int position) {}
+    void insertAtBegin(T data) {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
 
-    void printList() {}
+    void insertAtEnd(T data) {
+        Node *newNode = new Node(data);
+
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node *current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+
+    void insertAtMiddle(T data, unsigned int position) {
+        if (position == 0) {
+            insertAtBegin(data);
+            return;
+        }
+
+        Node *newNode = new Node(data);
+        Node *current = head;
+        unsigned int currentPos = 0;
+
+        while (current != nullptr && currentPos < position - 1) {
+            current = current->next;
+            currentPos++;
+        }
+
+        if (current == nullptr) {
+            std::cout << "Position out of bounds\n";
+            delete newNode;
+            return;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    void deleteAtBegin() {
+        if (head == nullptr) {
+            std::cout << "List is empty\n";
+            return;
+        }
+
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void deleteAtEnd() {
+        if (head == nullptr) {
+            std::cout << "List is empty\n";
+            return;
+        }
+
+        if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+
+        Node *current = head;
+        while (current->next->next != nullptr) {
+            current = current->next;
+        }
+
+        delete current->next;
+        current->next = nullptr;
+    }
+
+    void deleteAtMiddle(unsigned int position) {
+        if (head == nullptr) {
+            std::cout << "List is empty\n";
+            return;
+        }
+
+        if (position == 0) {
+            deleteAtBegin();
+            return;
+        }
+
+        Node *current = head;
+        unsigned int currentPos = 0;
+
+        while (current != nullptr && currentPos < position - 1) {
+            current = current->next;
+            currentPos++;
+        }
+
+        if (current == nullptr || current->next == nullptr) {
+            std::cout << "Position out of bounds\n";
+            return;
+        }
+
+        Node *temp = current->next;
+        current->next = temp->next;
+        delete temp;
+    }
+
+    int search(T target) {
+        if (head == nullptr) {
+            return -1;
+        }
+
+        Node *current = head;
+        int position = 0;
+
+        while (current != nullptr) {
+            if (current->data == target) {
+                return position;
+            }
+            current = current->next;
+            position++;
+        }
+
+        return -1;
+    }
+
+    void printList() {
+        Node *current = head;
+
+        if (current == nullptr) {
+            std::cout << "List is empty\n";
+            return;
+        }
+
+        while (current != nullptr) {
+            std::cout << current->data;
+            if (current->next != nullptr) {
+                std::cout << " -> ";
+            }
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
 #endif
