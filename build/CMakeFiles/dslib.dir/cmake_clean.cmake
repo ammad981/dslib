32b8a1efa16dabc9db2b09671655649f6c5b2b81
Ammad981 <mammadqureshi981@gmail.com>
2024-12-29 19:34:23 +0500

    coded menu ui for remianing Data Structures in main.cpp file

---
+++ build/CMakeFiles/dslib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dslib.dir/main.cpp.o"
+  "CMakeFiles/dslib.dir/main.cpp.o.d"
+  "dslib"
+  "dslib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dslib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
